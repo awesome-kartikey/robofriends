69e585daf29c1f214d09222881bad60c0a9e5b07
Kartikey Kumar <46820893+awesome-kartikey@users.noreply.github.com>
2023-12-04 16:54:56 +0530

    RoboFriends by Kartikey ZTM

---
+++ src/containers/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox'
+import './App.css'
+import Scroll from '../components/Scroll'
+
+
+
+class App extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+
+        this.onSearchChange = this.onSearchChange.bind(this);
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }))
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+        // console.log(event.target.value);
+    }
+
+
+    render() {
+        const { robots, searchfield } = this.state;
+        const filterRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+
+        })
+        return !robots.length ?
+            <h1 className='tc v-mid'>Loading</h1>
+            :
+            (
+                <div className='tc'>
+                    <h1 className='f1'>R o b o F r i e n d s</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filterRobots} />
+                    </Scroll>
+                </div>
+            );
+
+    }
+}
+
+export default App
+
+
